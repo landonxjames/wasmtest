7fa7e47b677fc9269f38b08738fd5ccae9f9b891
Landon James <lnj@amazon.com>
2023-07-31 14:25:37 -0700

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,11 @@ use aws_smithy_client::erase::DynConnector;
 use aws_smithy_http::{body::SdkBody, result::ConnectorError};
 use fluvio_wasm_timer;
 use std::time::Duration;
-use wasm_bindgen::{prelude::*, JsCast};
+use wasm_bindgen::prelude::*;
 
 extern crate console_error_panic_hook;
 
-//TODO: replace this with something that will log from WASI runtimes?
+//TODO: replace this with something that will log from non JS hosts envs?
 macro_rules! log {
     ( $( $t:tt )* ) => {
         web_sys::console::log_1(&format!( $( $t )* ).into());
@@ -23,8 +23,6 @@ pub async fn list_tables() -> Result<String, String> {
     console_error_panic_hook::set_once();
 
     let credentials_provider = static_credential_provider();
-    let credentials = credentials_provider.provide_credentials().await.unwrap();
-    let access_key = credentials.access_key_id();
 
     let shared_config = aws_config::from_env()
         .time_source(WasmTimeSource)
@@ -48,7 +46,6 @@ pub async fn list_tables() -> Result<String, String> {
         .send()
         .await
         .map_err(|e| format!("{:?}", e))?;
-    log!("Panics before here");
 
     let tables = resp.table_names().unwrap_or_default();
 
@@ -97,12 +94,16 @@ fn static_credential_provider() -> impl ProvideCredentials {
 fn wasm_credentials_cache() -> CredentialsCache {
     let shared_sleep = SharedAsyncSleep::new(WasmSleep);
     let shared_time = SharedTimeSource::new(WasmTimeSource);
+    //TODO: the time_source shouldn't actually be set here, should be inherited from
+    //config now that issue is fixed. Need to build from smithy-rs main to test
     CredentialsCache::lazy_builder()
         .sleep(shared_sleep)
         .time_source(shared_time)
         .into_credentials_cache()
 }
 
+//Code below here is all dedicated to sending the call across the sandbox barrier
+//to be executed by the host runtime
 #[derive(Debug, Clone)]
 struct Adapter {
     verbose: bool,
@@ -114,6 +115,7 @@ impl Adapter {
     }
 }
 
+//Replacing the default call impl for the SDK
 impl tower::Service<http::Request<SdkBody>> for Adapter {
     type Response = http::Response<SdkBody>;
 
@@ -142,38 +144,26 @@ impl tower::Service<http::Request<SdkBody>> for Adapter {
             log!("");
         }
 
-        let (tx, rx) = tokio::sync::oneshot::channel();
+        let (send_channel, rec_channel) = tokio::sync::oneshot::channel();
 
         log!("begin request...");
         wasm_bindgen_futures::spawn_local(async move {
             let fut = WasmHttpClient::send(parts, body);
-
-            // let blah = Box::pin(
-            //     reqwest::Client::new()
-            //         .request(req.method().clone(), req.uri().clone().to_string())
-            //         .body(body.bytes().unwrap())
-            //         .headers(req.headers().clone())
-            //         .send(),
-            // );
-
-            let _ = tx.send(fut.await.unwrap_or_else(|val| {
+            let _ = send_channel.send(fut.await.unwrap_or_else(|val| {
                 panic!("failure while making request to: {} \n {:#?}", uri, val)
             }));
         });
 
         Box::pin(async move {
-            let response = rx.await.map_err(|e| ConnectorError::user(Box::new(e)))?;
+            let response = rec_channel
+                .await
+                .map_err(|e| ConnectorError::user(Box::new(e)))?;
             log!("response received");
             Ok(response)
         })
     }
 }
 
-//TODO: Figure out how to do this in a WASI runtime
-/// At this moment, there is no standard mechanism to make an outbound
-/// HTTP request from within the guest Wasm module.
-/// Eventually that will be defined by the WebAssembly System Interface:
-/// https://github.com/WebAssembly/wasi-http
 #[async_trait(?Send)]
 trait MakeRequestWasm {
     async fn send(
@@ -184,82 +174,27 @@ trait MakeRequestWasm {
 
 pub struct WasmHttpClient;
 
-//TODO This might need a node-fetch shim?
 #[async_trait(?Send)]
 impl MakeRequestWasm for WasmHttpClient {
-    /// The [Fetch API](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API)
-    /// will be used to actually send the outbound HTTP request.
-    /// Most of the logic here is around converting from
-    /// the [http::Request]'s shape to [web_sys::Request].
     async fn send(
         parts: http::request::Parts,
         body: SdkBody,
     ) -> Result<http::Response<SdkBody>, JsValue> {
-        // use js_sys::{Array, ArrayBuffer, Reflect, Uint8Array};
-        // use wasm_bindgen_futures::JsFuture;
-
-        // let mut opts = web_sys::RequestInit::new();
-        // opts.method(parts.method.as_str());
-        // opts.mode(web_sys::RequestMode::Cors);
-
-        // let body_pinned = std::pin::Pin::new(body.bytes().unwrap());
-        // if body_pinned.len() > 0 {
-        //     let uint_8_array = unsafe { Uint8Array::view(&body_pinned) };
-        //     opts.body(Some(&uint_8_array));
-        // }
-
-        // let request = web_sys::Request::new_with_str_and_init(&parts.uri.to_string(), &opts)?;
-
-        // for (name, value) in parts
-        //     .headers
-        //     .iter()
-        //     .map(|(n, v)| (n.as_str(), v.to_str().unwrap()))
-        // {
-        //     request.headers().set(name, value)?;
-        // }
-
-        // let window = web_sys::window().ok_or("could not get window")?;
-        // let promise = window.fetch_with_request(&request);
-        // let res_web = JsFuture::from(promise).await?;
-        // let res_web: web_sys::Response = res_web.dyn_into().unwrap();
-
-        // let promise_array = res_web.array_buffer()?;
-        // let array = JsFuture::from(promise_array).await?;
-        // let buf: ArrayBuffer = array.dyn_into().unwrap();
-        // let slice = Uint8Array::new(&buf);
-        // let body = slice.to_vec();
-
-        // let mut builder = http::Response::builder().status(res_web.status());
-        // for i in js_sys::try_iter(&res_web.headers())?.unwrap() {
-        //     let array: Array = i?.into();
-        //     let values = array.values();
-
-        //     let prop = String::from("value").into();
-        //     let key = Reflect::get(values.next()?.as_ref(), &prop)?
-        //         .as_string()
-        //         .unwrap();
-        //     let value = Reflect::get(values.next()?.as_ref(), &prop)?
-        //         .as_string()
-        //         .unwrap();
-        //     builder = builder.header(&key, &value);
-        // }
-        // let res_body = SdkBody::from(body);
-        // let res = builder.body(res_body).unwrap();
-        // Ok(res)
         let body_bytes = body.bytes().unwrap().to_vec();
-
-        let blah = reqwest::Client::new()
+        //Reqwest uses wasm-bindgen under the hood to bind calls to fetch
+        //https://github.com/seanmonstar/reqwest/blob/61b1b2b5e6dace3733cdba291801378dd974386a/src/wasm/client.rs#L12
+        let res = reqwest::Client::new()
             .request(parts.method, parts.uri.to_string())
             .body(body_bytes)
             .headers(parts.headers)
             .send()
             .await
             .unwrap_or_else(|err| panic!("failure while making request: {}", err));
-        let mut builder = http::Response::builder().status(blah.status());
-        let blah_body = SdkBody::from(blah.bytes().await.unwrap());
 
-        let blah_res = builder.body(blah_body).unwrap();
+        let builder = http::Response::builder().status(res.status());
+        let sdk_body = SdkBody::from(res.bytes().await.unwrap());
+        let sdk_res = builder.body(sdk_body).unwrap();
 
-        Ok(blah_res)
+        Ok(sdk_res)
     }
 }
